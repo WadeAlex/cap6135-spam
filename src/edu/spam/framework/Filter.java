6619b3024b8fa05dd3268b2f73207c3a85a0b9a6
WadeAlex <WadeAlex@98544896-27c8-11de-968e-2d0d980aa813>
2009-04-17 03:51:31 +0000

    Got a crappy basic bayesian filter in.  Oh yeah, and it's not tested...

--- src/edu/spam/framework/Filter.java
+++ src/edu/spam/framework/Filter.java
@@ -1,8 +1,11 @@
 package edu.spam.framework;
 
+import java.io.IOException;
+
+import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
 public interface Filter {
-	public boolean train(MimeMessage msg);
-	public boolean test(MimeMessage msg);
+	public boolean train(MimeMessage msg, boolean spam);
+	public float test(MimeMessage msg) throws IOException, MessagingException;
 }

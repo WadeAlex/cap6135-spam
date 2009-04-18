ffbacc2866a0091eb1eaf2d8ec713f7653cd5de9
qbproger <qbproger@98544896-27c8-11de-968e-2d0d980aa813>
2009-04-18 18:15:32 +0000

    added the stemmer + bayesian filter (oddly lowered accuracy)

---
+++ src/edu/spam/framework/filters/StemBayFilter.java
@@ -0,0 +1,27 @@
+package edu.spam.framework.filters;
+
+import java.io.IOException;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.tartarus.snowball.SnowballStemmer;
+import org.tartarus.snowball.ext.englishStemmer;
+
+public class StemBayFilter extends BayesianFilter {
+
+	protected String[] getTokens(MimeMessage msg) throws IOException, MessagingException {
+		String[] words = super.getTokens(msg);
+		
+		SnowballStemmer stemmer = new englishStemmer(); 
+		
+		for(int i=0; i<words.length; i++) {
+			stemmer.setCurrent(words[i]);
+			stemmer.stem();
+			words[i] = stemmer.getCurrent();
+		}
+		
+		return words;
+	}
+	
+}
